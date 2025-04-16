6d130814b8d9635fb51fde3a3f828f16b8fc015b
Yogesh Jat <81260380+yogii2002@users.noreply.github.com>
2025-04-16 15:58:47 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@
 #!/bin/sh
 
 
-exec ${JAVA_HOME}/bin/java ${JAVA_OPTIONS} -jar ${FUSEKI_JAR} --file=databases/test_kg/test_kg.ttl /test_kg
+exec ${JAVA_HOME}/bin/java ${JAVA_OPTIONS} -jar ${FUSEKI_JAR} --port=80 --file=databases/test_kg/test_kg.ttl /test_kg
